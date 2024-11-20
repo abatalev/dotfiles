53960269a473839441937a736a452e58bb3ba3ea
andrey <andrey.batalev@gmail.com>
2024-11-20 18:13:48 +0300

    battery: change the color to red when the battery level is low

--- .local/bin/battery.sh
+++ .local/bin/battery.sh
@@ -4,9 +4,13 @@ PERC=$(cat /sys/class/power_supply/BAT0/capacity)
 STAT=$(cat /sys/class/power_supply/BAT0/status)
 
 if [ "${STAT}" == "Charging" ]; then
-    echo -e "\udc85 %{F#AAAAAA}*${PERC}%%%{F-}" 
+    echo -e "%{F#AAAAAA}\udc85 *${PERC}%%%{F-}" 
 elif [ "${STAT}" == "Not charging" ]; then
-    echo -e "\ueb2d %{F#AAAAAA}${PERC}%%%{F-}"
+    echo -e "%{F#AAAAAA}\ueb2d ${PERC}%%%{F-}"
 else
-    echo -e "\uf240 %{F#AAAAAA}${PERC}%%%{F-}"
+    if [ $PERC -gt 15 ]; then
+        echo -e "%{F#AAAAAA}\uf240 ${PERC}%%%{F-}"
+    else 
+        echo -e "%{F#AA0000}\uf240 ${PERC}%%%{F-}"
+    fi
 fi
