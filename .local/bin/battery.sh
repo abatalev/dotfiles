3c02a989c00059c39c4f14eba050dcf1e7b22de9
andrey <andrey.batalev@gmail.com>
2024-11-20 17:27:15 +0300

    battery: new look

--- .local/bin/battery.sh
+++ .local/bin/battery.sh
@@ -4,7 +4,9 @@ PERC=$(cat /sys/class/power_supply/BAT0/capacity)
 STAT=$(cat /sys/class/power_supply/BAT0/status)
 
 if [ "${STAT}" == "Charging" ]; then
-    echo "*${PERC}" 
+    echo -e "\udc85 %{F#AAAAAA}*${PERC}%%%{F-}" 
+elif [ "${STAT}" == "Not charging" ]; then
+    echo -e "\ueb2d %{F#AAAAAA}${PERC}%%%{F-}"
 else
-    echo " ${PERC}"
+    echo -e "\uf240 %{F#AAAAAA}${PERC}%%%{F-}"
 fi
