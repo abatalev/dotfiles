fdc8ab206865ad7b5dec86e7eef9efad3087b0b1
andrey <andrey.batalev@gmail.com>
2024-12-06 16:17:28 +0300

    modify work with notes

---
+++ .local/bin/notes/editall.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+zk edit --interactive --sort=modified-
--- .local/bin/syncthing/status.sh
+++ .local/bin/syncthing/status.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-echo -e "%{F#AAAAAA}\uf2f1%{F-}"
+status=$(ps aux | awk '{ print $11; }' | grep "/usr/bin/syncthing" | wc -l)
+if [ "x$status" == "x1" ]; then
+    echo -e "%{F#00AA00}\uf2f1%{F-}"
+else
+    echo -e "%{F#AA0000}\uf2f1%{F-}"
+fi
