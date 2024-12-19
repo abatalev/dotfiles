7e541eb73828d58a7e4eca2ce3f468fa37361ea1
andrey <andrey.batalev@gmail.com>
2024-12-19 09:08:30 +0300

    succade: modify date show

---
+++ .local/bin/date/status.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+CDATE=$(date +'%d.%m %H:%M')
+echo -e "%{F#AAAAAA}\uf017${CDATE}%{F-}"
--- .local/bin/weather/status.sh
+++ .local/bin/weather/status.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-# json https://wttr.in/spb?format=j1
+#json https://wttr.in/spb?format=j1
 res=$(curl -s "https://wttr.in/spb?M&format=2")
 echo -e "$res"
+#echo "-"
