d517be52ab7d066e66e1a772beeab5916016ebb1
andrey <andrey.batalev@gmail.com>
2025-05-21 11:54:53 +0300

    modify pkgupd.sh

--- pkgupd.sh
+++ pkgupd.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 pacman -Q | awk '{ print $1; }' > packages.current
-
-diff -U0 packages.current packages.txt | awk '
-/+/ { print substr($0,1); }
-'
+HOSTNAME="$(uname -n)"
+grep -v -f "${HOSTNAME}.skip.txt" packages.txt > packages.filtered.txt
+diff -U0 packages.current packages.filtered.txt | awk '
+/^+/ { 
+  if (substr($0,1,4) != "+++ ") {
+	print substr($0,2); 
+  }
+}
+' > "packages.delta.${HOSTNAME}"
 rm packages.current
+rm packages.filtered.txt
