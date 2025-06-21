a43f5dead243dd028890d677ef9af2e0367b7f9a
andrey <andrey.batalev@gmail.com>
2025-06-21 21:17:00 +0300

    git-info: added list of branches

--- .local/bin/git-info.sh
+++ .local/bin/git-info.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 echo -e "\e[32m--[*]-[ git log ]-----\e[0m"
-git -P log --graph --oneline | head -n 10
+git -P log --graph --oneline --decorate | head -n 10
 echo -e "\e[32m--[*]-[ git remote ]--\e[0m"
 git remote -v
+echo -e "\e[32m--[*]-[ git branch ]--\e[0m"
+git -P branch -a
 echo -e "\e[32m--[*]-[ git status ]--\e[0m"
 git status
 echo -e "\e[32m--[*]-[ done ]--------\e[0m"
