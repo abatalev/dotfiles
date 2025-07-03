a0a79e4b13f1409675cd8ce29e4da81a90339b55
andrey <andrey.batalev@gmail.com>
2025-07-03 09:21:33 +0300

    git-nifo: uniq remotes

--- .local/bin/git-info.sh
+++ .local/bin/git-info.sh
@@ -2,7 +2,7 @@
 echo -e "\e[32m--[*]-[ git log ]-----\e[0m"
 git -P log --graph --oneline --decorate | head -n 10
 echo -e "\e[32m--[*]-[ git remote ]--\e[0m"
-git remote -v
+git remote -v | awk '{ print $1" "$2; }' | uniq
 echo -e "\e[32m--[*]-[ git branch ]--\e[0m"
 git -P branch -a
 echo -e "\e[32m--[*]-[ git status ]--\e[0m"
